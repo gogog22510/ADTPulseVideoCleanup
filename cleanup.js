cdc47f67f4c39b1afc14c7ce3f7f6f1893ee9a3a
Charles <gogog22510@gmail.com>
2023-07-26 21:11:40 -0700

    Update cleanup.js

--- cleanup.js
+++ cleanup.js
@@ -1,16 +1,17 @@
 var count = 0;
-var adtpulse_url = "https://portal.adtpulse.com/myhome/6.18.0-238/pictures/editpictures/serv/DelPictureServ";
-var sat = findsat();
-
-function findsat() {
-    var jq = $('#delb').attr('onclick');
-    return jq.substring(jq.indexOf('(')+2,jq.indexOf(',')-1);
+function deleteVideo() {
+    var realConfirm=window.confirm;
+    window.confirm=function(){
+      window.confirm=realConfirm;
+      return true;
+    };
+    $('.deleteMediaBtn').click();
 }
 
 function deleteVideoTest() {
     if(count > 0) {
         count--;
-        $.post(adtpulse_url, {video:1, sat:sat}, function(data) { console.log(data); });
+        deleteVideo();
         setTimeout(deleteVideoTest, 1000);
     }
 }
